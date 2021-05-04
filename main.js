bb89e2bf9799a60dc50c47da32c22b89330e91d9
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-05-04 19:48:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function goFirst() {
+	window.location = "";
+}
+
+function goSecond() {
+	window.location = "";
+}
+
+function goThird() {
+	window.location = "";
+}
+
+function goFourth() {
+	window.location = "";
+}
+
+function goFifth() {
+	window.location = "";
+}
